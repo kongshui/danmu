088d106a0725242d7da4a61ad7828abba0fb7cc1
kongkongkong <luofenghan@163.com>
2025-12-17 15:56:01 +0800

    fixed 选边互动失败后，不添加roundid至CurrentRoundId

--- service/websocketFunc.go
+++ service/websocketFunc.go
@@ -98,20 +98,23 @@ func roundStart(msg *pmsg.MessageBody) error {
 	// 延迟调用选边
 	switch platform {
 	case "ks":
+		isok := true
 		t := time.NewTimer(time.Second * 2)
 		<-t.C
 		if syncGameStatusData.ChooseSide {
 			if interactive != nil {
 				if !interactive(syncGameStatusData.RoomId, strconv.FormatInt(syncGameStatusData.RoundId, 10), int8(syncGameStatusData.SideType)) {
+					isok = false
 					ziLog.Error(fmt.Sprintf("round start, roomid:: %v,主播openid %v,设置自动选边互动失败", syncGameStatusData.RoomId, syncGameStatusData.AnchorOpenId), debug)
 				}
 			}
 		}
 		//添加roundid至CurrentRoundId
-		if err := liveCurrentRoundAdd(syncGameStatusData.RoomId, syncGameStatusData.RoundId); err != nil {
-			return errors.New("uplink liveCurrentRoundAdd err: " + err.Error())
+		if isok {
+			if err := liveCurrentRoundAdd(syncGameStatusData.RoomId, syncGameStatusData.RoundId); err != nil {
+				return errors.New("uplink liveCurrentRoundAdd err: " + err.Error())
+			}
 		}
-
 	case "dy":
 
 	}
